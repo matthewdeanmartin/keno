1e88682621d593b56a90a987066cca7cb2ab2242
Matthew Martin <matthewdeanmartin@gmail.com>
2018-06-24 16:56:04 -0400

    don't create 10gb file on travis

--- keno/number_machine.py
+++ keno/number_machine.py
@@ -2,7 +2,10 @@
 """
 Draw numbers...fast.
 """
+
 import os
+
+import random
 from itertools import cycle
 from typing import List, Iterator
 
@@ -13,7 +16,6 @@ except:
     print("We don't have numpy, simulation will be slower, but startup time might be a bit faster")
     HAVE_NUMPY = False
 
-import random
 
 EIGHTY_NUMBERS = list(range(1, 80))
 
@@ -66,7 +68,10 @@ class NumbersMachine(object):
         # fastest
         global GENERATOR
         global EIGHTY_NUMBERS
-        drawing = next(GENERATOR)
+        if is_travis:
+            drawing = self.draw_no_caching()
+        else:
+            drawing = next(GENERATOR)
         # shave down to numbers we need.
         drawing = drawing[0:self.spots]
         list.sort(drawing)
@@ -141,13 +146,17 @@ def generate_lots_of_numbers() -> Iterator[List[int]]:
                 continue
             yield value # 6.2, 6.05, 6.23, 6.38, 5.74
 
-GENERATOR = cycle(generate_lots_of_numbers())
 
-# fix seeding on multithreading
-skips = random.randint(0, 5000)
-print("skipping : " + str(skips))
-for _ in range(0, skips):
-    next(GENERATOR)
+is_travis = 'TRAVIS' in os.environ
+if is_travis:
+    GENERATOR = None
+else:
+    GENERATOR = cycle(generate_lots_of_numbers())
+    # fix seeding on multithreading
+    skips = random.randint(0, 5000)
+    print("skipping : " + str(skips))
+    for _ in range(0, skips):
+        next(GENERATOR)
 
 if __name__ == "__main__":
     def run() -> None:
